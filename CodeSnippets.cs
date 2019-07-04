420fecdccbb1e330a0b31dbac0336d2b807a321e
SamuelJRMarshall <sam.marshall014@gmail.com>
2019-07-04 17:08:47 +0100

    Add CodeSnippets
    From Pluralsight course, note worthing code which I want to try at home

---
+++ CodeSnippets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayDaysOfWeek
+{
+    class CodeSnippets
+    {
+        //Call this method from the streamreader
+        public Student ReadStudentFromFile(string line)
+        {
+            string[] parts = line.Split(new char[] { ',' });
+            return new Student(parts[0], parts[1], parts[2], int.Parse(parts[3]));
+        }
+        public int HowtoTryParse(string parse)
+        {
+            int.TryParse(parse, out int value);
+            return value;
+        }
+    }
+}
